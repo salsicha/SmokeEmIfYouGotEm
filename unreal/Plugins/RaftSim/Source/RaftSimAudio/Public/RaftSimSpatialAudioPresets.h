f6d908b9a0a1d7bea997057bbdd50d0733b61c89
Alex Moran <salsicha@gmail.com>
2026-06-28 13:45:37 -0700

    Define spatial audio presets

--- unreal/Plugins/RaftSim/Source/RaftSimAudio/Public/RaftSimSpatialAudioPresets.h
+++ unreal/Plugins/RaftSim/Source/RaftSimAudio/Public/RaftSimSpatialAudioPresets.h
@@ -9,9 +9,14 @@ UENUM(BlueprintType)
 enum class ERaftSimSpatializationMode : uint8
 {
     PointSource,
-    LargeWaterSource,
+    LineAreaWaterSource,
+    LargeRapid,
     AmbisonicBed,
+    OccludedBank,
+    RaftContact,
+    UnderwaterNearWater,
     CrewVoice,
+    GuideCommand,
     MultiplayerVoice
 };
 
@@ -33,10 +38,25 @@ struct FRaftSimSpatialAudioPreset
     float SourceSpreadMeters = 0.0f;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RaftSim|SpatialAudio")
+    float NearFieldGainDb = 0.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RaftSim|SpatialAudio")
+    int32 AmbisonicOrder = 0;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RaftSim|SpatialAudio")
+    FName MixBus;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RaftSim|SpatialAudio")
+    float OcclusionLowPassHz = 1800.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RaftSim|SpatialAudio")
     bool bUseBinauralForHeadphonesAndVR = true;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RaftSim|SpatialAudio")
     bool bUseReverbAndOcclusion = true;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RaftSim|SpatialAudio")
+    bool bHeadLockedInVR = false;
 };
 
 UCLASS(BlueprintType)
