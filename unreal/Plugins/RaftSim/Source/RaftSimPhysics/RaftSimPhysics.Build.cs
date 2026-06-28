341e14816f6a0f4ed534acbdbbae95b5d67b8aa4
Alex Moran <salsicha@gmail.com>
2026-06-28 05:06:44 -0700

    Integrate custom water runtime candidate

--- unreal/Plugins/RaftSim/Source/RaftSimPhysics/RaftSimPhysics.Build.cs
+++ unreal/Plugins/RaftSim/Source/RaftSimPhysics/RaftSimPhysics.Build.cs
@@ -1,4 +1,5 @@
 using UnrealBuildTool;
+using System.IO;
 
 public class RaftSimPhysics : ModuleRules
 {
@@ -6,5 +7,9 @@ public class RaftSimPhysics : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "RaftSimCore" });
+
+        PublicDefinitions.Add("RAFTSIM_WATER_RUNTIME_NAME=\"raftsim_water_cpp_v1\"");
+        PublicDefinitions.Add("RAFTSIM_WATER_RUNTIME_HEADER_ONLY_BRIDGE=1");
+        PublicIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "../../../../../physics/cpp/include")));
     }
 }
