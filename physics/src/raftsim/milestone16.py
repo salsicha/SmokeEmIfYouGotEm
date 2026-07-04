2a666a8ebc9533ea8ecf769aee4d29c9f5c7524e
Alex Moran <salsicha@gmail.com>
2026-07-03 18:14:48 -0700

    Close reduced South Fork median parity

--- physics/src/raftsim/milestone16.py
+++ physics/src/raftsim/milestone16.py
@@ -2584,6 +2584,8 @@ def _cpp_config_for_mode(
         preserve_initial_mass = False
     if not finite_volume and gate_scenario_id == "south_fork_low_runnable":
         preserve_initial_mass = False
+    if not finite_volume and gate_scenario_id == "south_fork_median_runnable":
+        preserve_initial_mass = False
     if finite_volume and gate_scenario_id == "bed_step":
         flux_scheme = "roe"
     return CppSolverRunConfig(
