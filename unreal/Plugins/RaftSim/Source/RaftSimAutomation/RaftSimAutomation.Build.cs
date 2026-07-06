4475b4576dba1b9a652c1f97fdc887a8de5c5286
Alex Moran <salsicha@gmail.com>
2026-07-05 17:12:25 -1000

    Complete Milestone 11 crew safety gate

--- unreal/Plugins/RaftSim/Source/RaftSimAutomation/RaftSimAutomation.Build.cs
+++ unreal/Plugins/RaftSim/Source/RaftSimAutomation/RaftSimAutomation.Build.cs
@@ -5,7 +5,7 @@ public class RaftSimAutomation : ModuleRules
     public RaftSimAutomation(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "RaftSimCore", "RaftSimWater", "RaftSimPhysics" });
+        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "RaftSimCore", "RaftSimWater", "RaftSimPhysics", "RaftSimCrew" });
         PrivateDependencyModuleNames.AddRange(new[] { "Json", "Projects" });
     }
 }
