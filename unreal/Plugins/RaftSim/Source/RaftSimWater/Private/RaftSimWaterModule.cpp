30dea9037202d9025505da403551253638ea5975
Alexander Lloyd Moran <salsicha@gmail.com>
2026-09-17 14:33:29 -0700

    Add opt-in eight-worker gameplay profiling control

--- unreal/Plugins/RaftSim/Source/RaftSimWater/Private/RaftSimWaterModule.cpp
+++ unreal/Plugins/RaftSim/Source/RaftSimWater/Private/RaftSimWaterModule.cpp
@@ -1,6 +1,8 @@
 #include "RaftSimWaterModule.h"
 
 #include "Modules/ModuleManager.h"
+#include "Misc/CommandLine.h"
+#include "Misc/Parse.h"
 #if RAFTSIM_HAS_LIVE_SOLVER
 #include "raftsim_water/solver.hpp"
 #endif
@@ -9,6 +11,14 @@ void FRaftSimWaterModule::StartupModule()
 {
 #if RAFTSIM_HAS_LIVE_SOLVER
     UE_LOG(LogTemp, Display, TEXT("RaftSim live-water solver archive: %hs"), RAFTSIM_LIVE_SOLVER_BUILD_SHA256);
+    // Temporary qualification control: never resize a running pool, alter a
+    // numerical partition/reduction, or silently enable this in saved gameplay.
+    if (FParse::Param(FCommandLine::Get(), TEXT("RaftSimEphemeralProfile")) &&
+        FParse::Param(FCommandLine::Get(), TEXT("RaftSimEightSolverLanes")))
+    {
+        raftsim::configure_solver_workers(8);
+        UE_LOG(LogTemp, Display, TEXT("RaftSim solver execution review: eight maximum lanes including caller"));
+    }
 #endif
 }
 
